88ef6c437e373a36df5646cdb3aada8ec0f8df26
Viktor <viab92@outlook.com>
2014-04-04 17:10:03 +0300

    04.04
    ClientDAO (пробник)

--- src/main/java/com/sgsoft/servicer/util/dao/ClientDAO.java
+++ src/main/java/com/sgsoft/servicer/util/dao/ClientDAO.java
@@ -1,7 +1,23 @@
 package com.sgsoft.servicer.util.dao;
 
+import com.sgsoft.servicer.db.exception.DBException;
+import com.sgsoft.servicer.entity.Client;
+
+import java.util.List;
+
 /**
  * Created by Viktor Rotar on 04.04.14.
  */
 public interface ClientDAO {
+
+    public List<Client> findClientsByName(String name) throws DBException;
+
+    public Client findById(Integer id) throws DBException;
+
+    public int saveClient(Client client) throws DBException;
+
+    public boolean isExists(String name, String email, String telephone) throws DBException;
+
+    public Client findByUniqueParams(String name, String email, String telephone) throws DBException;
+
 }
